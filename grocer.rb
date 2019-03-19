7d6da60949fe22a0f6884abef9395d4d608282d2
Natasha Chernis <natashachernis@gmail.com>
2019-03-19 20:00:10 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -46,5 +46,7 @@ def apply_clearance(cart)
 end
 
 def checkout(cart, coupons)
-  
+  consolidated_cart = consolidate_cart(cart)
+  couponed_cart = apply_coupons(consolidated_cart)
+  discounted_cart = apply_clearance(couponed_cart)
 end
